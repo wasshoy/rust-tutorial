a895d2583db11c5d284fddf16f973c44043deb83
A.Arimura <404ari@gmail.com>
2020-08-29 16:01:38 +0900

    🦀 カニ7カニ

---
+++ ch7/communicator/src/main.rs
@@ -0,0 +1,24 @@
+extern crate communicator;
+// use a::series::of::nested_modules;
+// use TrafficLight::*;
+// use TrafficLight::{Red, Yellow};
+
+fn main() {
+    // communicator::client::connect();
+    // nested_modules();
+    // let red = Red;
+    // let green = TrafficLight::Green;
+}
+
+// pub mod a {
+//     pub mod series {
+//         pub mod of {
+//             pub fn nested_modules() {}
+//         }
+//     }
+// }
+// enum TrafficLight {
+//     Red,
+//     Yellow,
+//     Green,
+// }
